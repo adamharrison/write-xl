8c7806385d21c25f9b9bed42739a6b5a75a550f2
Adam Harrison <adamdharrison@gmail.com>
2022-04-17 20:00:24 -0400

    Removed bottom bar, added in user folder to make it write-xl.

--- android/com.litexl.writexl/app/src/main/java/com/litexl/writexl/writexlActivity.java
+++ android/com.litexl.writexl/app/src/main/java/com/litexl/writexl/writexlActivity.java
@@ -9,40 +9,50 @@ import java.io.IOException;
 import android.os.Bundle;
 import android.util.Log;
 import android.os.Environment;
+import android.view.View;
+import android.app.ActionBar;
+import android.view.WindowManager;
+import android.view.WindowInsetsController;
+import android.view.WindowInsets;
 import java.io.File;
 
 public class writexlActivity extends SDLActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String prefix = getExternalFilesDir(null) + "";
+        String userdir = getExternalFilesDir("user") + "";
         try {
-            copyDirectoryOrFile(getAssets(), "data");
+            copyDirectoryOrFile(getAssets(), "data", getExternalFilesDir("share") + "/lite-xl");
+            copyDirectoryOrFile(getAssets(), "user", userdir + "/lite-xl");
         } catch (IOException e) {
             Log.e("assetManager", "Failed to copy assets: " + e.getMessage());
         }
         super.onCreate(savedInstanceState);
-        String prefix = getExternalFilesDir(null) + "";
         Log.i("litexl", "Setting LITE_PREFIX to " + prefix);
         nativeSetenv("LITE_PREFIX", prefix);
         Log.i("litexl", "Setting HOME to " + prefix);
         nativeSetenv("HOME", prefix);
         Log.i("litexl", "Setting LITE_SCALE to 1.0");
-        nativeSetenv("LITE_SCALE", "1.0");
-        String userdir = prefix + "/user";
-        File directory = new File(userdir);
-        if (!directory.exists() && !directory.mkdir())
-            Log.e("assetManager", "Can't make directory " + userdir);
+        nativeSetenv("LITE_SCALE", "2.0");
         Log.i("litexl", "Setting XDG_CONFIG_HOME to " + userdir);
         nativeSetenv("XDG_CONFIG_HOME", userdir);
+        
+        getWindow().setDecorFitsSystemWindows(false);
+        WindowInsetsController controller = getWindow().getInsetsController();
+        if (controller != null) {
+            controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+            controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        }
     }
     
-    private void copyDirectoryOrFile(AssetManager assetManager, String path) throws IOException {
-        Log.i("assetManager", "Copying assets from " + path);
-        String[] files = assetManager.list(path);
-        String target = getExternalFilesDir("share") + "/lite-xl/" + path.replace("data/", "");
+    private void copyDirectoryOrFile(AssetManager assetManager, String source, String target) throws IOException {
+        Log.i("assetManager", "Copying assets from " + source);
+        String[] files = assetManager.list(source);
+        String transformedTarget = target + "/" + source.substring(source.indexOf("/") + 1);
         if (files.length == 0) {
-            Log.i("assetManager", "Copying file " + path + " to " + target);
-            InputStream in = assetManager.open(path);
-            FileOutputStream out = new FileOutputStream(target);
+            Log.i("assetManager", "Copying file " + source + " to " + transformedTarget);
+            InputStream in = assetManager.open(source);
+            FileOutputStream out = new FileOutputStream(transformedTarget);
             byte[] buffer = new byte[1024];
             int read;
             while((read = in.read(buffer)) != -1){
@@ -52,12 +62,12 @@ public class writexlActivity extends SDLActivity {
             out.flush();
             out.close();
         } else {
-            Log.i("assetManager", "Copying directory " + path + " to " + target);
-            File directory = new File(target);
-            if (!directory.exists() && !directory.mkdir())
-                throw new IOException("Can't make directory " + target);
+            Log.i("assetManager", "Copying directory " + source + " to " + transformedTarget);
+            File directory = new File(transformedTarget);
+            if (!directory.exists() && !directory.mkdirs())
+                throw new IOException("Can't make directory " + transformedTarget);
             for (String file : files)
-                copyDirectoryOrFile(assetManager, path != "" ? path + "/" + file : file);
+                copyDirectoryOrFile(assetManager, source != "" ? source + "/" + file : file, target);
         }
     }
 }
