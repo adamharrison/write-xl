d8aa633540892b29991101b7fc7fc4d8e499311e
Adam Harrison <adamdharrison@gmail.com>
2023-03-28 16:19:08 -0400

    Added in auto-release action.

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 
 export BIN=`pwd`/write-xl.apk
 export LITEXL_REPOS="file://`pwd`:main https://github.com/adamharrison/lite-xl-libgit2.git"
-export LITEXL_PLUGINS="write-xl plugin_manager"
+export LITEXL_PLUGINS="write-xl"
 cp -r template lib/lite-xl-android && cd lib/lite-xl-android && ./build.sh
+
 
